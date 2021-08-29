44b01f0a2dde5c847d7edf89b3714ddf7294ccb0
Patrice LASSEY <splassey@gmail.com>
2021-08-29 11:35:43 +0200

    Mise a jour test : testParkingACar()

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,10 @@
 package com.parkit.parkingsystem.integration;
-
+import junit.framework.Assert;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
+
+import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -48,14 +51,28 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingACar(){
+    public void testParkingACar() throws Exception{
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-    }
+        /* check that a ticket is actualy saved in DB */
+		Ticket ticket = ticketDAO.getTicket(inputReaderUtil.readVehicleRegistrationNumber());
+		Boolean ticketIsCheck = ticket.getVehicleRegNumber()
+				.contentEquals(inputReaderUtil.readVehicleRegistrationNumber());
+		Assert.assertTrue(ticketIsCheck);
+		Assert.assertNull(ticket.getOutTime());
+		Assert.assertTrue( ticket.getPrice()==0);
+		Assert.assertFalse(ticket.getParkingSpot().isAvailable());
+		Boolean ticketInTimeIsCheck = ticket.getInTime().before(new Date());
+		Assert.assertTrue(ticketInTimeIsCheck);
+
+		/* check that Parking table is updated with availability */
+		Boolean parkingSpotIsUpdated = parkingSpotDAO.updateParking(ticket.getParkingSpot());
+		Assert.assertTrue(parkingSpotIsUpdated);
+        }
 
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() throws Exception{
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
