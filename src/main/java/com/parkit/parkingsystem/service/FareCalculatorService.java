8f41b97c1cf8fe72eb4700718ff1bd0ef4f39814
Patrice LASSEY <splassey@gmail.com>
2021-08-29 11:45:59 +0200

    Correction de la methode de calcul du prix de stationnement

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,58 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+		int inDay = ticket.getInTime().getDate();
+		int outDay = ticket.getOutTime().getDate();
+		int inHour = ticket.getInTime().getHours();
+		int outHour = ticket.getOutTime().getHours();
+		int inMinutes = ticket.getInTime().getMinutes();
+		int outMinutes = ticket.getOutTime().getMinutes();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+		// TODO: Some tests are failing here. Need to check if this logic is correct
+		int duration = ((outDay * 24 * 60) + (outHour * 60) + outMinutes)
+				- ((inDay * 24 * 60) + (inHour * 60) + inMinutes);
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		switch (ticket.getParkingSpot().getParkingType()) {
+		case CAR: {
+			if (duration < 60) {
+				ticket.setPrice(0.75 * Fare.CAR_RATE_PER_HOUR);
+
+				break;
+			} else if ((duration < 24 * 60) && (duration >= 60)) {
+				ticket.setPrice((duration / 60) * Fare.CAR_RATE_PER_HOUR);
+
+				break;
+			} else if ((duration >= 24 * 60)) {
+				System.out.println("duree>=24");
+				System.out.println(duration);
+				ticket.setPrice((duration / (24 * 60)) * 24 * Fare.CAR_RATE_PER_HOUR);
+
+				break;
+			}
+		}
+		case BIKE: {
+			if (duration < 60) {
+				ticket.setPrice(0.75 * Fare.BIKE_RATE_PER_HOUR);
+
+				break;
+			} else if ((duration < 24 * 60)) {
+				ticket.setPrice((duration / 60) * Fare.BIKE_RATE_PER_HOUR);
+
+				break;
+			} else if ((duration >= 24 * 60)) {
+				System.out.println("duree>=24");
+				System.out.println(duration);
+				ticket.setPrice((duration / 24) * 24 * Fare.BIKE_RATE_PER_HOUR);
+				break;
+			}
+		}
+		default:
+			throw new IllegalArgumentException("Unkown Parking Type");
+		}
+	}
 }
